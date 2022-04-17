772e4110b4508f5be5f01244175fc8ee42a68594
Deepthi <deepthi251@gmail.com>
2022-04-17 17:36:54 +0530

    Validations added for Register form

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,36 +9,37 @@ import { DataService } from '../service/data.service';
 export class DashboardComponent implements OnInit {
 
 
- acno=""
- pswd=""
- amount=""
+  acno = ""
+  pswd = ""
+  amount = ""
 
- acno1=""
- pswd1=""
- amount1=""
+  acno1 = ""
+  pswd1 = ""
+  amount1 = ""
 
-  constructor(private ds:DataService) { }
+  constructor(private ds: DataService) { }
 
   ngOnInit(): void {
   }
-deposit(){
-  var acno=this.acno
-  var pswd=this.pswd
-  var amount=this.amount
+  deposit() {
+    var acno = this.acno
+    var pswd = this.pswd
+    var amount = this.amount
+    // caliing deposit in dataservice
+    const result = this.ds.deposit(acno, pswd, amount)
+    if (result) {
+      alert(amount + "successfully deposited..And new balance is " + result)
+    }
+  }
+  withdraw() {
+    var acno = this.acno1
+    var pswd = this.pswd1
+    var amount = this.amount1
+    // calling withdraw in dataservice
+    const result = this.ds.withdraw(acno, pswd, amount)
+    if (result) {
+      alert(amount + "Successfully debited.. New balance is :" + result)
+    }
 
- const result= this.ds.deposit(acno, pswd, amount)
- if(result){
-   alert(amount + "successfully deposited..And new balance is " +result)
- }
-}
-withdraw(){
-  var acno=this.acno1
-  var pswd=this.pswd1
-  var amount=this.amount1
-  const result=this.ds.withdraw(acno,pswd,amount)
- if(result){
-   alert(amount + "Successfully debited.. New balance is :"+ result)
- }
-  
-}
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../service/data.service';
 
@@ -9,30 +9,39 @@ import { DataService } from '../service/data.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registerForm=this.fb.group({
 
-  uname:[''],
-  acno:[''],
-  pswd:['']
-  
-})
-  constructor(private db:DataService,private router:Router,private fb:FormBuilder) { }
+  // register form model
+
+
+  registerForm = this.fb.group({
+
+    uname: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+    acno: [''],
+    pswd: ['']
+
+  })
+  constructor(private db: DataService, private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
-register(){
+  register() {
 
-  var acno=this.registerForm.value.acno
-  var pswd=this.registerForm.value.pswd
-  var uname=this.registerForm.value.uname
-  const result = this.db.register(uname,acno,pswd)
-  if(result){
-    alert("Successfully registered!!!!")
-    this.router.navigateByUrl("")
+    var acno = this.registerForm.value.acno
+    var pswd = this.registerForm.value.pswd
+    var uname = this.registerForm.value.uname
+
+
+    if (this.registerForm.valid) {
+      const result = this.db.register(uname,  acno, pswd)
+      if (result) {
+        alert("Successfully registered!!!!")
+        this.router.navigateByUrl("")
+      }
+      else {
+        alert("account already exist..Please Login!!!")
+      }
+    } else {
+      alert("Invalid Form!!!!")
+    }
   }
-  else{
-    alert("account already exist..Please Login!!!")
-  }
-  
-}
 }
